75fb87a2c8ad6a5184ec70502740c592720a2400
Egor Chernooky <e.chernooky@gmail.com>
2022-04-20 17:35:50 +0300

    add repo annotation

--- src/main/java/com/example/jwtoken/repository/MarkRepository.java
+++ src/main/java/com/example/jwtoken/repository/MarkRepository.java
@@ -3,7 +3,9 @@ package com.example.jwtoken.repository;
 import com.example.jwtoken.model.Mark;
 import com.example.jwtoken.model.MarkKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarkRepository extends JpaRepository<Mark, MarkKey> {
 
 }
